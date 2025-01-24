e2429be5a8209360bf565c665e1fa2c7fffb8d6e
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2025-01-23 23:31:42 -0500

    Update search_schema.py

--- app/schemas/search_schema.py
+++ app/schemas/search_schema.py
@@ -3,5 +3,3 @@ from pydantic import BaseModel, Field, EmailStr
 class SearchSchema(BaseModel):
     id_card: str = Field(..., example="123456789", min_length=1, max_length=50)
     ip_address: str = Field(..., example="127.0.0.1", min_length=1, max_length=50)
-    # email: EmailStr = Field(..., example="alanturing@enigmasas.com")
-    # phone: str = Field(..., example="1234567890", min_length=7, max_length=15)
